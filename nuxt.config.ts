782b45051b8f4eec333ba7d8026c4901c4e2ce8a
Yoan Marchal <marchalyoan@gmail.com>
2023-07-09 16:57:05 +0200

    look if zero config is default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,6 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/alpine
   extends: '@nuxt-themes/alpine',
-  routeRules: {
-    // Homepage pre-rendered at build time
-    '/': { prerender: true },
-    // Blog post generated on-demand once until next deploy
-    '/articles/**': { isr: true },
-    // Admin dashboard renders only on client-side
-    '/admin/**': { ssr: false }
-  },
-
   modules: [
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible',
